8bb9b4c2d6732df780009387654d023d38e24784
Sebastian Cain <sebastian.cain.stuy@gmail.com>
2017-02-16 00:56:33 -0500

    commit for hw due thurs

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from display import *
 from draw import *
+import random
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-for i in range(25):
-    draw_line(screen, 0, 0, 500, i*20, color)  
+for i in range(100):
+    draw_line(screen, 250, 250, int(random.random() * 500), int(random.random() * 500), color)  
+'''
+draw_line(screen, 250, 250, 100, 0, color)
+draw_line(screen, 120, 0, 250, 250, color)
+'''
+color = [ 255, 0, 255 ]
+draw_line(screen, 250, 250, 0, 0, color)
+draw_line(screen, 250, 250, 0, 250, color)
+draw_line(screen, 250, 250, 0, 500, color)
+draw_line(screen, 250, 250, 250, 500, color)
+draw_line(screen, 250, 250, 500, 500, color)
+draw_line(screen, 250, 250, 500, 250, color)
+draw_line(screen, 250, 250, 500, 0, color)
+draw_line(screen, 250, 250, 250, 0, color)
 
 display(screen)
 save_extension(screen, 'img.png')
